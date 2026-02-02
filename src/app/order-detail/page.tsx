c279724e16689373dd748084fa43ab0fd3253e49
ymarturelo <yslemarturelo@gmail.com>
2026-02-02 07:42:04 -0500

    feat: add order details page with price, status and links

---
+++ src/app/order-detail/page.tsx
@@ -0,0 +1,9 @@
+import OrderDetail from "../__components/OrderDetail";
+
+export default function OrderDetailPage() {
+  return (
+    <main className="grid px-4 min-h-dvh place-items-center">
+      <OrderDetail />
+    </main>
+  );
+}
---
+++ src/app/order-details/page.tsx
@@ -0,0 +1,9 @@
+import OrderDetails from "../__components/OrderDetail";
+
+export default function OrderDetailsPage() {
+  return (
+    <main className="grid px-4 min-h-dvh place-items-center">
+      <OrderDetails />
+    </main>
+  );
+}
