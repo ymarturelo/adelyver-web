f1ca29a1080c77dbf8fbef7158605c74052063b8
ymarturelo <yslemarturelo@gmail.com>
2026-02-15 00:20:18 -0500

    feat: add admin login panel and connect it

---
+++ src/app/admin-login-panel/page.tsx
@@ -0,0 +1,8 @@
+import AdminLoginForm from "./adminLoginForm";
+
+export default function AdminLoginPanelPage() {
+  return <main className="grid px-10 min-h-dvh place-items-center">
+    <AdminLoginForm />
+  </main>;
+}
+    
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,9 @@
 import Login from "./Login";
 
-
 export default function LoginPage() {
-  return <main className="grid px-10 min-h-dvh place-items-center">
-    <Login />
-  </main>;
+  return (
+    <main className="grid px-10 min-h-dvh place-items-center">
+      <Login />
+    </main>
+  );
 }
-    
