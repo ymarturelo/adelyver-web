8081d581699aa997a222cc94c1198ca3dbe91323
kolostring <yoerodriguezjunior@gmail.com>
2026-03-14 19:25:44 -0400

    feat: add login link to AuthGuard when error

--- src/app/__components/AuthGuard.tsx
+++ src/app/__components/AuthGuard.tsx
@@ -1,5 +1,7 @@
 import { isAuthenticated } from "@/features/actions/ClientsController.actions";
+import Link from "next/link";
 import { redirect } from "next/navigation";
+import { Button } from "./ui/button";
 
 export default async function AuthGuard({
   children,
@@ -13,8 +15,10 @@ export default async function AuthGuard({
       <header className="grid place-content-center text-center gap-4 justify-items-center h-dvh">
         <h1 className="text-h1">Problemas obteniendo permisos</h1>
         <p className="text-lead max-w-[40ch]">
-          Por favor prueba a recargar la página o comunícate con el
-          administrador.
+          Por favor prueba a recargar la página o{" "}
+          <Button variant="link" asChild className="text-xl">
+            <Link href="/auth/login">vuelve a autenticarte</Link>
+          </Button>
         </p>
         <p>
           Detalles: {isAuthenticatedRes.error.message}{" "}
