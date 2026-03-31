42416428f50f75840b5e153a7f216c7fbfa21a5c
kolostring <yoerodriguezjunior@gmail.com>
2026-03-31 16:33:23 -0400

    Revert "preview commit for testing deploy"
    This reverts commit 7243a87efd9c819f25329e2f350905b1bcc14bd7.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import "./globals.css";
 import { Toaster } from "sonner";
 import QueryProvider from "./__providers/QueryProvider";
 
-/** preview */
 export default function RootLayout({
   children,
 }: Readonly<{
